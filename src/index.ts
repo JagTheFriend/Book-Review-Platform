7ae76e83cb592ecaa8f84b83cf70128064008a68
JagTheFriend <JagTheFriend12@gmail.com>
2025-07-15 13:37:05 +0530

    Created Prisma Schema

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,27 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+// Retrieve all books with pagination
+app.get("/books", async (c) => { })
+
+// Retrieve a specific book
+app.get("/books/:id", async (c) => { })
+
+// Add a new book (ADMIN Only)
+app.post("/books", async (c) => { })
+
+// Get reviews for a book
+app.get("/reviews", async (c) => { })
+
+// Submit a new review
+app.post("/reviews", async (c) => { })
+
+// Get user
+app.get("/users/:userId", async (c) => { })
+
+// Update user
+app.put("/users/:userId", async (c) => { })
+
 serve({
   fetch: app.fetch,
   port: 3000
