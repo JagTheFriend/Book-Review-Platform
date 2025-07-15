0cb72c175f9f22739f37ac5d940027c4300cf4ec
JagTheFriend <JagTheFriend12@gmail.com>
2025-07-15 15:04:20 +0530

    Integrated Zod validation and Prisma ORM to perform validation, queries etc

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { serve } from '@hono/node-server'
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
+import z, { string } from 'zod'
+import { db } from './db.js'
 
 const app = new Hono()
 
@@ -8,25 +11,114 @@ app.get('/', (c) => {
 })
 
 // Retrieve all books with pagination
-app.get("/books", async (c) => { })
+app.get("/books",
+  zValidator("query", z.object({
+    skip: z.number().min(0).default(0),
+    limit: z.number().min(1).max(50).default(10)
+  })),
+  async (c) => {
+    const { skip, limit } = c.req.valid('query')
+    const data = await db.book.findMany({
+      skip,
+      take: limit,
+    })
+    return c.json({ data }, 200)
+  })
 
 // Retrieve a specific book
-app.get("/books/:id", async (c) => { })
+app.get("/books/:id",
+  zValidator("param", z.object({
+    id: z.string()
+  })),
+  async (c) => {
+    const { id } = c.req.valid('param')
+    const data = await db.book.findUnique({
+      where: { id },
+
+    })
+    return c.json({ data }, 200)
+  })
 
 // Add a new book (ADMIN Only)
-app.post("/books", async (c) => { })
+app.post("/books",
+  zValidator("json", z.object({
+    name: z.string(),
+    description: z.string(),
+    author: z.string(),
+    tags: z.array(z.string()),
+    userId: z.string()
+  })),
+  async (c) => {
+    const { name, description, author, tags, userId } = c.req.valid('json')
+    const user = await db.user.findUnique({ where: { id: userId } })
+    if (user?.role !== 'ADMIN') return c.json({ error: 'Unauthorized' }, 401);
+
+    const data = await db.book.create({
+      data: { name, description, author, tags, userId }
+    })
+    return c.json({ data }, 200)
+  })
 
 // Get reviews for a book
-app.get("/reviews", async (c) => { })
+app.get("/reviews/:bookId",
+  zValidator("param", z.object({
+    bookId: z.string()
+  })),
+  async (c) => {
+    const { bookId } = c.req.valid('param')
+    const data = await db.review.findMany({ where: { bookId } })
+    return c.json({ data }, 200)
+  })
 
 // Submit a new review
-app.post("/reviews", async (c) => { })
+app.post("/reviews",
+  zValidator("json", z.object({ data: z.string(), userId: z.string(), bookId: z.string() })),
+  async (c) => {
+    const { data, userId, bookId } = c.req.valid('json')
+    const review = await db.review.create({
+      data: {
+        data,
+        userId,
+        bookId
+      }
+    })
+    return c.json({ review }, 200)
+  })
 
 // Get user
-app.get("/users/:userId", async (c) => { })
+app.get("/users/:userId",
+  zValidator("param", z.object({
+    userId: z.string()
+  })),
 
-// Update user
-app.put("/users/:userId", async (c) => { })
+  async (c) => {
+    const { userId } = c.req.valid('param')
+    return c.json({ data: await db.user.findUnique({ where: { id: userId } }) }, 200)
+  }
+)
+
+// Create/Update user
+app.put("/users/:userId",
+  zValidator("param", z.object({ userId: z.string() })),
+  zValidator("json", z.object({
+    username: z.string(),
+    role: z.enum(['ADMIN', 'USER'])
+  })),
+  async (c) => {
+    const { userId } = c.req.valid('param')
+    const { username, role } = c.req.valid('json')
+
+    const user = await db.user.upsert({
+      where: { id: userId },
+      update: { username, role },
+      create: {
+        username,
+        role
+      }
+    })
+
+    return c.json({ data: user }, 200)
+  })
 
 serve({
   fetch: app.fetch,
